11961fe7dd323172a916f0f71e0a692ffbd39324
alexei <artalexeimail@gmail.com>
2021-12-02 18:32:01 +0300

    add args

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,4 +1,6 @@
+import argparse
 import json
+import os
 from urllib.parse import urljoin
 
 import requests
@@ -7,13 +9,22 @@ from bs4 import BeautifulSoup
 from main import download_image, download_txt, parse_book_page, check_for_redirect
 
 books = []
-for page in range(1, 5):
+parser = argparse.ArgumentParser(description='Download books from tululu.org')
+parser.add_argument('--start_page', default=1, type=int)
+parser.add_argument('--end_page', default=701, type=int)
+parser.add_argument('--dest_folder', default=os.getcwd(), type=str, help='path to the directory with parsing results')
+parser.add_argument('--skip_imgs', default=False, type=bool, help='do not download images')
+parser.add_argument('--skip_txt', default=False, type=bool, help='do not download books')
+parser.add_argument('--json_path', default=os.getcwd(), type=str, help='specify your path to *.json file with results')
+
+args = parser.parse_args()
+for page in range(args.start_page, args.end_page):
     url = f'https://tululu.org/l55/{page}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
 
-    books_from_page=soup.select('.d_book')
+    books_from_page = soup.select('.d_book')
     for book in books_from_page:
         href = book.select_one('a').get('href')
         book_id = href[2:-1]
@@ -23,17 +34,20 @@ for page in range(1, 5):
             response.raise_for_status()
             check_for_redirect(response)
             book = parse_book_page(response.text)
-
-            url = 'https://tululu.org/txt.php'
-            params = {'id': book_id}
-            book['file_path'] = download_txt(url, params, f'{book_id}.{book["title"]}')
-
-            url = urljoin('https://tululu.org/', book["img_src"])
-            download_image(url)
+            if not args.skip_txt:
+                url = 'https://tululu.org/txt.php'
+                params = {'id': book_id}
+                book['file_path'] = download_txt(url,
+                                                 params,
+                                                 f'{book_id}.{book["title"]}',
+                                                 os.path.join(args.dest_folder, 'books/'))
+            if not args.skip_imgs:
+                url = urljoin('https://tululu.org/', book["img_src"])
+                download_image(url, os.path.join(args.dest_folder, 'images/'))
         except requests.HTTPError:
             continue
         books.append(book)
         print(book)
 
-with open("books.json", "w", encoding='utf-8') as file:
+with open(os.path.join(args.json_path, 'books.json'), "w", encoding='utf-8') as file:
     json.dump(books, file, ensure_ascii=False)
